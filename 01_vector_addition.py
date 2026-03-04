5f15deb3de8cbe7fb3204fb91093b217b7baedf1
Janaki Ram Puli <janakirampuli@gmail.com>
2026-03-04 09:10:37 +0000

    added vector addition triton

---
+++ 01_vector_addition.py
@@ -0,0 +1,93 @@
+import torch
+import triton
+import triton.language as tl
+import sys
+
+DEVICE = torch.device(f'cuda:{torch.cuda.current_device()}')
+
+@triton.jit
+def add_kernel(
+    x_ptr, # pointer to start of x
+    y_ptr,
+    output_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr
+):
+    # program id
+    # 1d launch kernel so axis = 0
+    pid = tl.program_id(axis=0)
+
+    block_start = pid * BLOCK_SIZE
+
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+
+    mask = offsets < n_elements
+
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+
+    output = x + y
+    tl.store(output_ptr + offsets, output, mask=mask)
+    
+
+def add(x, y):
+    output = torch.empty_like(x)
+
+    # grid
+    n_elements = output.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), 1)
+
+    add_kernel[grid](
+        x,
+        y,
+        output,
+        n_elements,
+        BLOCK_SIZE=1024
+    )
+
+    return output
+
+def test_add_kernel(size, atol=1e-3, rtol=1e-3, device=DEVICE):
+    torch.manual_seed(0)
+    x = torch.randn(size, device=DEVICE)
+    y = torch.randn(size, device=DEVICE)
+
+    z_triton = add(x, y)
+    z_pytorch = x + y
+
+    torch.testing.assert_close(z_triton, z_pytorch, atol=atol, rtol=rtol)
+    print('test passed')
+
+@triton.testing.perf_report(
+    triton.testing.Benchmark(
+        x_names=['size'],
+        x_vals=[2**i for i in range(12, 28, 1)],
+        x_log=True,
+        line_arg='provider',
+        line_vals=['triton', 'torch'],
+        line_names=['Triton', 'Torch'],
+        styles=[('blue', '-'), ('green', '-')],
+        ylabel='GB/s',
+        plot_name='vector-add-performance',
+        args={}
+    )
+)
+def benchmark(size, provider):
+    x = torch.randn(size, device=DEVICE, dtype=torch.float32)
+    y = torch.randn(size, device=DEVICE, dtype=torch.float32)
+
+    quantiles = [0.5, 0.05, 0.95]
+    if provider == 'torch':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: x + y, quantiles=quantiles)
+    if provider == 'triton':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: add(x, y), quantiles=quantiles)
+
+    gbps = lambda ms: 3 * x.numel() * x.element_size() * 1e-9 / (ms * 1e-3)
+    return (gbps(ms), gbps(min_ms), gbps(max_ms))
+
+if __name__ == "__main__":
+    test_add_kernel(4096)
+    test_add_kernel(4097)
+
+    if len(sys.argv) > 1 and sys.argv[1] == '--benchmark':
+        benchmark.run(save_path='.', print_data=True)
